bbad4ef391acea0ad40c388633202d4d685dcc61
pm <pm@101media.ru>
2024-03-11 17:09:20 +0300

    add tag blockquote

--- src/TelegramChannel.php
+++ src/TelegramChannel.php
@@ -72,7 +72,7 @@ class TelegramChannel
             }
 
             $message['text'] = strip_tags($message['text'],
-                '<b><strong><i><em><u><ins><s><strike><del><span><tg-spoiler><a><tg-emoji><code><pre>'
+                '<b><strong><i><em><u><ins><s><strike><del><span><tg-spoiler><a><tg-emoji><code><pre><blockquote>'
             );
 
             return $message;
